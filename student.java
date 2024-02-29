6f3e599120fc0dd43d2ddf7872be0026a8109157
VanTam <12a5.vovantam@gmail.com>
2024-02-29 19:56:52 +0700

    bbbb

---
+++ student.java
@@ -0,0 +1,20 @@
+
+public class student {
+    private String id;
+
+    private String name;
+
+    private String add;
+
+
+    public String getName(){
+        return name;
+    }
+    public String getId(){
+        return id;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("stop");
+    }
+}
