a29518b1bddcf146feb6295c7177df5eb8d82ff2
haluongtan <160831939+haluongtan@users.noreply.github.com>
2024-10-05 19:06:25 +0700

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -8,42 +8,25 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApp8
+namespace bai5
 {
-    public partial class Form1 : Form
+    public partial class frmQuanLySinhVien : Form
     {
-        public Form1()
+        public frmQuanLySinhVien()
         {
             InitializeComponent();
         }
 
-        private void openToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog oFile = new OpenFileDialog();
-            oFile.Filter = "Bitmap file|*.bmp|JPEG file|*.jpg";
-            if (oFile.ShowDialog() == DialogResult.OK)
-            {
-                Form2 frm = new Form2(oFile.FileName); 
-                frm.MdiParent = this;
-                frm.Show();
-            }
-        }
+      
 
-
-        private void casedeToolStripMenuItem_Click(object sender, EventArgs e)
+        private void btnThem_Click(object sender, EventArgs e)
         {
-            LayoutMdi(MdiLayout.Cascade);
 
         }
 
-        private void tileToolStripMenuItem_Click(object sender, EventArgs e)
+        private void btnXoa_Click(object sender, EventArgs e)
         {
-            LayoutMdi(MdiLayout.TileHorizontal);
-        }
 
-        private void tileToolStripMenuItem1_Click(object sender, EventArgs e)
-        {
-            LayoutMdi(MdiLayout.TileVertical);
         }
     }
 }
